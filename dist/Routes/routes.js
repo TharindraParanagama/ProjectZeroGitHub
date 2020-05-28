429cb8ea6d24174f3899bf766bb8d0ca7f61a196
Tharindra Paranagama <tharindrapara@gmail.com>
2020-05-27 21:10:03 -0700

    adjusted for CORS

--- dist/Routes/routes.js
+++ dist/Routes/routes.js
@@ -15,77 +15,78 @@ exports.path.use(middleware_1.requestTracker);
 exports.path.use(middleware_1.bp);
 exports.path.use(middleware_1.sess);
 //route to the landing page with a get request
-exports.path.get('/landingPage', function (req, res) {
-    res.send('Hi Welcome to the book store\n Please login to proceed.Check');
+exports.path.get("/landingPage", function (req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.send("Hi Welcome to the book store\n Please login to proceed.Check");
 });
-//route to the login portal with the user credentials to be specified in the 
+//route to the login portal with the user credentials to be specified in the
 //body of the request
-exports.path.post('/login', middleware_1.auth);
+exports.path.post("/login", middleware_1.auth);
 //multiple roles
-exports.path.get('/role', middleware_1.validator, function (req, res) {
+exports.path.get("/role", middleware_1.validator, function (req, res) {
     if (req.session.role === "admin") {
-        res.send('Welcome to admin portal');
+        res.send("Welcome to admin portal");
     }
     else if (req.session.role === "vendor") {
-        res.send('Welcome to vendor portal');
+        res.send("Welcome to vendor portal");
     }
     else {
-        res.send('Welcome to customer portal');
+        res.send("Welcome to customer portal");
     }
 });
 //route to search section
-exports.path.get('/search', middleware_1.validator, function (req, res) {
-    connection_1.db.any('SELECT * FROM book_catalog')
+exports.path.get("/search", middleware_1.validator, function (req, res) {
+    connection_1.db.any("SELECT * FROM book_catalog")
         .then((result) => {
         res.json(result);
     })
         .catch((error) => {
-        res.send('Sorry your search result does not match with any of our records!');
+        res.send("Sorry your search result does not match with any of our records!");
     });
 });
 //route to obtain the details for all the book supplied by a given supplier
 //affiliated with my store
-exports.path.get('/ID/:supplier_id', middleware_1.validator, function (req, res) {
+exports.path.get("/ID/:supplier_id", middleware_1.validator, function (req, res) {
     let url = req.params;
-    connection_1.db.any('SELECT * FROM book_catalog WHERE supplier_id=${supplier_id}', {
-        supplier_id: url.supplier_id
+    connection_1.db.any("SELECT * FROM book_catalog WHERE supplier_id=${supplier_id}", {
+        supplier_id: url.supplier_id,
     })
         .then((result) => {
         res.json(result);
     })
         .catch((error) => {
-        res.send('Sorry your search result does not match with any of our records!');
+        res.send("Sorry your search result does not match with any of our records!");
     });
 });
 //route to obtain title of books which is higher that a given supplier rating
-exports.path.get('/rating/:supplier_rating', middleware_1.validator, function (req, res) {
+exports.path.get("/rating/:supplier_rating", middleware_1.validator, function (req, res) {
     let url = req.params;
-    connection_1.db.any('SELECT title FROM book_catalog INNER JOIN supplier ON book_catalog.supplier_id=supplier.supplier_id WHERE supplier_rating > ${supplier_rating}', {
-        supplier_rating: url.supplier_rating
+    connection_1.db.any("SELECT title FROM book_catalog INNER JOIN supplier ON book_catalog.supplier_id=supplier.supplier_id WHERE supplier_rating > ${supplier_rating}", {
+        supplier_rating: url.supplier_rating,
     })
         .then((result) => {
         res.json(result);
     })
         .catch((error) => {
-        res.send('No records found');
+        res.send("No records found");
     });
 });
-//route for new joiners        
-exports.path.post('/signup', function (req, res) {
-    connection_1.db.none('INSERT INTO members VALUES(${username},${password},${role})', {
+//route for new joiners
+exports.path.post("/signup", function (req, res) {
+    connection_1.db.none("INSERT INTO members VALUES(${username},${password},${role})", {
         username: req.body.username,
         password: req.body.password,
-        role: req.body.role
+        role: req.body.role,
     })
         .then((result) => {
-        res.send('Please login to continue');
+        res.send("Please login to continue");
     })
         .catch((error) => {
-        res.send('please input valid information!');
+        res.send("please input valid information!");
     });
 });
 //route for a session termination
-exports.path.get('/logout', function (req, res) {
+exports.path.get("/logout", function (req, res) {
     req.session.destroy();
-    res.redirect('/landingPage');
+    res.redirect("/landingPage");
 });
--- src/Routes/routes.ts
+++ src/Routes/routes.ts
@@ -1,115 +1,109 @@
 //importing modules
-import express from 'express'
-import {db} from '../DB Connection/connection'
-import {sess,requestTracker,bp,auth,validator} from '../Middleware/middleware'
+import express from "express";
+import { db } from "../DB Connection/connection";
+import {
+  sess,
+  requestTracker,
+  bp,
+  auth,
+  validator,
+} from "../Middleware/middleware";
 
 //creating a router instance
-export const path:any=express.Router()
+export const path: any = express.Router();
 
 //using the middleware
-path.use(requestTracker)
-path.use(bp)
-path.use(sess)
-
+path.use(requestTracker);
+path.use(bp);
+path.use(sess);
 
 //route to the landing page with a get request
-path.get('/landingPage',function(req:any,res:any){
+path.get("/landingPage", function (req: any, res: any) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.send("Hi Welcome to the book store\n Please login to proceed.Check");
+});
 
-    res.send('Hi Welcome to the book store\n Please login to proceed.Check')
-    
-})
-
-//route to the login portal with the user credentials to be specified in the 
+//route to the login portal with the user credentials to be specified in the
 //body of the request
-path.post('/login',auth)
+path.post("/login", auth);
 
 //multiple roles
-path.get('/role',validator,function(req:any,res:any){
-    if(req.session.role==="admin"){    
-        res.send('Welcome to admin portal')
-    }else if(req.session.role==="vendor"){
-        res.send('Welcome to vendor portal')
-    }else{
-        res.send('Welcome to customer portal')
-    }
- })
-
+path.get("/role", validator, function (req: any, res: any) {
+  if (req.session.role === "admin") {
+    res.send("Welcome to admin portal");
+  } else if (req.session.role === "vendor") {
+    res.send("Welcome to vendor portal");
+  } else {
+    res.send("Welcome to customer portal");
+  }
+});
 
 //route to search section
-path.get('/search',validator,function(req:any,res:any){
-
-    db.any('SELECT * FROM book_catalog')
-     .then((result: any)=>{
-         res.json(result)
-     })
-     .catch((error: any) => {
-         res.send('Sorry your search result does not match with any of our records!')
-     })
-   
-
-})
+path.get("/search", validator, function (req: any, res: any) {
+  db.any("SELECT * FROM book_catalog")
+    .then((result: any) => {
+      res.json(result);
+    })
+    .catch((error: any) => {
+      res.send(
+        "Sorry your search result does not match with any of our records!"
+      );
+    });
+});
 
 //route to obtain the details for all the book supplied by a given supplier
 //affiliated with my store
-path.get('/ID/:supplier_id',validator,function(req:any,res:any){
-   
-    let url:any=req.params
+path.get("/ID/:supplier_id", validator, function (req: any, res: any) {
+  let url: any = req.params;
 
-    db.any('SELECT * FROM book_catalog WHERE supplier_id=${supplier_id}',{
-       supplier_id:url.supplier_id
-    })
-    .then((result: any)=>{
-        res.json(result)
+  db.any("SELECT * FROM book_catalog WHERE supplier_id=${supplier_id}", {
+    supplier_id: url.supplier_id,
+  })
+    .then((result: any) => {
+      res.json(result);
     })
     .catch((error: any) => {
-        res.send('Sorry your search result does not match with any of our records!')
-    })
-
-})
+      res.send(
+        "Sorry your search result does not match with any of our records!"
+      );
+    });
+});
 
 //route to obtain title of books which is higher that a given supplier rating
-path.get('/rating/:supplier_rating',validator,function(req:any,res:any){
+path.get("/rating/:supplier_rating", validator, function (req: any, res: any) {
+  let url: any = req.params;
 
-    let url:any=req.params
-
-    db.any('SELECT title FROM book_catalog INNER JOIN supplier ON book_catalog.supplier_id=supplier.supplier_id WHERE supplier_rating > ${supplier_rating}',{
-       supplier_rating:url.supplier_rating
-    })
-    .then((result: any)=>{
-        res.json(result)
+  db.any(
+    "SELECT title FROM book_catalog INNER JOIN supplier ON book_catalog.supplier_id=supplier.supplier_id WHERE supplier_rating > ${supplier_rating}",
+    {
+      supplier_rating: url.supplier_rating,
+    }
+  )
+    .then((result: any) => {
+      res.json(result);
     })
     .catch((error: any) => {
-        res.send('No records found')
-    })
-
- })
-
-//route for new joiners        
-path.post('/signup',function(req:any,res:any){
-
-    db.none('INSERT INTO members VALUES(${username},${password},${role})',{
-        username:req.body.username,
-        password:req.body.password,
-        role:req.body.role  
-    })
-    .then((result:any)=> {
-        res.send('Please login to continue')
-    })
-    .catch((error:any) => {
-        res.send('please input valid information!')
+      res.send("No records found");
     });
- 
-})
+});
+
+//route for new joiners
+path.post("/signup", function (req: any, res: any) {
+  db.none("INSERT INTO members VALUES(${username},${password},${role})", {
+    username: req.body.username,
+    password: req.body.password,
+    role: req.body.role,
+  })
+    .then((result: any) => {
+      res.send("Please login to continue");
+    })
+    .catch((error: any) => {
+      res.send("please input valid information!");
+    });
+});
 
 //route for a session termination
-path.get('/logout',function(req:any,res:any){
-    
-    req.session.destroy()
-    res.redirect('/landingPage')
-    
-
-})
-
-
-
-    
+path.get("/logout", function (req: any, res: any) {
+  req.session.destroy();
+  res.redirect("/landingPage");
+});
