de6653ce8dd75846194e897d44da5931d93958df
Tharindra Paranagama <tharindrapara@gmail.com>
2020-05-29 06:55:16 -0700

    test

--- src/Routes/routes.ts
+++ src/Routes/routes.ts
@@ -20,7 +20,7 @@ path.use(sess);
 //route to the landing page with a get request
 path.get("/landingPage", function (req: any, res: any) {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.send("Hi Welcome to the book store\n Please login to proceed.");
+  res.send("Hi Welcome to the book store\n Please login to proceed.Hi");
 });
 
 //route to the login portal with the user credentials to be specified in the
