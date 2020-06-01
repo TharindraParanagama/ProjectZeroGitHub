bab86617a7305b11cbdaa9d8e2ca3f77eb086940
Tharindra Paranagama <tharindrapara@gmail.com>
2020-05-31 19:55:31 -0700

    test

--- dist/server.js
+++ dist/server.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const main_1 = require("./main");
 //Allocating a port to run the server
-const PORT = 5000;
+const PORT = 3000;
 //a port designated for the server and a callback function
-// with an output to the console to know the server is up 
+// with an output to the console to know the server is up
 //and running
 main_1.app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import {app} from './main'
+import { app } from "./main";
 
 //Allocating a port to run the server
-const PORT:number =5000
+const PORT: number = 3000;
 
 //a port designated for the server and a callback function
-// with an output to the console to know the server is up 
+// with an output to the console to know the server is up
 //and running
-app.listen(PORT,()=>{
-    console.log(`Listening on port ${PORT}`)
-})  
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
